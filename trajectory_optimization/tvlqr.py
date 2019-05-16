fix display quaternion order

trajectory_optimization/tvlqr.py
@@ -4,10 +4,12 @@ import os
 import numpy as np
 from pydrake.systems.meshcat_visualizer import MeshcatVisualizer
 from pydrake.systems.framework import VectorSystem, DiagramBuilder
+from pydrake.systems.controllers import LinearQuadraticRegulator
 
 from ..simulate.simulate import simulate
 
 from ..models.quadrotor import Quadrotor
+from ..autodiff.linearize import QuadrotorDynamicsLinearizer
 from utilities import load_traj
 
 
@@ -26,6 +28,7 @@ class TVLQR(VectorSystem):
         assert(self.time_array.shape[0] == self.u_traj.shape[0]+1)
         self.final_time = self.time_array[-1][0]
         self.initial_state = self.traj[0]
+        self.dynamics_linearizer = QuadrotorDynamicsLinearizer()
 
     def DoCalcVectorOutput(self, context, u, x, y):
         """
@@ -39,19 +42,35 @@ class TVLQR(VectorSystem):
         index = np.where(self.time_array <= time)[0][-1]
 
         # test if reached terminal time
-        # if index < self.u_traj.shape[0]:
-        #     u = self.u_traj[index]
-        # else:
-        #     u = np.zeros_like(self.u_traj[0])
-
-        state = self.traj[index]
-        current_state = u
-        # print(state)
-        # print(current_state)
-        # print("================")
+        if index < self.u_traj.shape[0]:
+            u0 = self.u_traj[index]
+        else:
+            u0 = self.u_traj[index-1]
 
         # Set as output, i.e control input to the quadrotor
-        y[:] = np.ones_like(self.u_traj[0])
+        current_state = u
+        target_state = self.traj[index]
+
+        # linearize around nominal point
+        A, B = self.dynamics_linearizer.get_AB(target_state, u0)
+
+        # Get Q and R cost matrices
+        Q = np.identity(12)
+        Q[:6, :6] = 10 * np.identity(6)
+        R = np.identity(4)
+
+        # Obtain controller through LQR
+        K, S = LinearQuadraticRegulator(A, B, Q, R)
+
+        # x_bar = x - x0
+        x_bar = np.array(current_state) - np.array(target_state)
+        x_bar = x_bar.reshape((-1, 1))
+
+        # u_bar = u - u0
+        u_bar = -np.matmul(K, x_bar).reshape((-1,))
+        out = u_bar + u0
+
+        y[:] = out
 
 
 if __name__ == "__main__":
@@ -72,7 +91,7 @@ if __name__ == "__main__":
     dir_path = os.path.dirname(os.path.realpath(__file__))
     filename = dir_path + "/traj/{}.npz".format(args.filename)
     controller = TVLQR(filename)
-    args.duration = controller.final_time
+    args.duration = controller.final_time + 5.0
 
     def initial_state_gen():
         return controller.initial_state

tutorial/block.py
@@ -22,12 +22,12 @@ class Block(LeafSystem):
         LeafSystem.__init__(self)
 
         # Declare inputs to the model.
-        # In this case, it is acceleration command
+        # (In this case, it is acceleration command) not used
         self.DeclareVectorInputPort(name="x", model_vector=BasicVector(1))
 
         # Declare state vector of the model
-        # Here we have x pos, and x vel
-        self.DeclareContinuousState(model_vector=BasicVector(np.zeros(2)), num_q=1, num_v=1, num_z=0)
+        # Here we have x, y, z, roll, pitch, yaw and their derivatives
+        self.DeclareContinuousState(model_vector=BasicVector(np.zeros(12)), num_q=6, num_v=6, num_z=0)
 
         # Output geometry state
         # To be fed through MultibodyPositionToGeometryPose to meshcat
@@ -43,16 +43,18 @@ class Block(LeafSystem):
         :param context: context for performing calculations
         :param output: output to be set
         """
-        # Obtain current state
-        x = context.get_continuous_state_vector().CopyToVector()
-
-        # Identity quaternion
         out = np.zeros((7,))
-        out[3] = 1
+
+        # Obtain current state
+        state = context.get_continuous_state_vector().CopyToVector()
+
+        # Convert roll-pitch-yaw to quaternion
+        quaternion_wxyz = RollPitchYaw(state[3:6]).ToQuaternion().wxyz()
+        out[:3] = quaternion_wxyz[1:]
+        out[3] = quaternion_wxyz[0]
 
         # set x and z pos
-        out[4] = x[0]
-        out[6] = 1
+        out[4:] = state[:3]
 
         # Send output
         output.SetFromVector(out)
@@ -62,15 +64,11 @@ class Block(LeafSystem):
         :param context: context for performing calculations
         :param derivatives: derivatives of the system to be set at current state
         """
-        # Get current state
-        x = context.get_continuous_state_vector().CopyToVector()
-
-        # Get input
-        u = self.EvalVectorInput(context, 0).GetAtIndex(0)
+        deriv = np.zeros((12,))
 
         # Set derivative of pos by current velocity,
         # and derivative of vel by input, which is acceleration
-        derivatives.get_mutable_vector().SetFromVector([x[1], u])
+        derivatives.get_mutable_vector().SetFromVector(deriv)
 
     def RegisterGeometry(self, scene_graph):
         """ Create the visual model of the system, and register in scene graph
@@ -135,7 +133,8 @@ context = simulator.get_mutable_context()
 
 for i in range(args.trials):
     context.set_time(0.)
-    # context.SetContinuousState(np.random.randn(2,))
-    context.SetContinuousState(np.zeros(2,))
+    initial_state = np.zeros((12,))
+    initial_state[3] = 0.7
+    context.SetContinuousState(initial_state)
     simulator.Initialize()
     simulator.StepTo(args.duration)

tutorial/height_controller.py
@@ -105,7 +105,9 @@ if __name__ == "__main__":
 
     for i in range(args.trials):
         context.set_time(0.)
-        context.SetContinuousState(np.zeros(shape=(12,)))
+        initial_state = np.zeros(shape=(12,))
+        initial_state[5] = 0.7
+        context.SetContinuousState(initial_state)
         simulator.Initialize()
         simulator.StepTo(args.duration)
 